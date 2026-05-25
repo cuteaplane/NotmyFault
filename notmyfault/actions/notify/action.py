66681fb13998423084728499ea09ed764cc8b07b
cuteaplane <124236930+cuteaplane@users.noreply.github.com>
2026-05-25 12:08:41 +0800

    Revert "Develop alpha"

--- notmyfault/actions/notify/action.py
+++
@@ -1,8 +0,0 @@
-from Win_toaster.show_notification import show_notification
-
-
-def run(action_info, params):
-    title = params.get("title", "NotmyFault")
-    message = params.get("message", "")
-    print(f"[Action:notify] 显示通知: {title} / {message}")
-    show_notification(title, message)
